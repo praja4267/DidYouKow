43d2620eed2ee814c2bcc39c036e04153f2fb6a7
Rajasekhar <praja4267@gmail.com>
2020-08-05 18:01:07 +0530

    Updated Country view controller to display tableview cells

--- KnowSomeThing/Screens/CountryScreen/View/CountryViewController.swift
+++ KnowSomeThing/Screens/CountryScreen/View/CountryViewController.swift
@@ -21,9 +21,16 @@ class CountryViewController: UIViewController {
     let presenter: CountryPresenter
     var viewModels: [ItemModel]?
     
+    let loader: UIActivityIndicatorView = {
+        let loader = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.gray)
+         loader.hidesWhenStopped = true
+        return loader
+    }()
+    
     init() {
         self.presenter = CountryPresenter()
         super.init(nibName: nil, bundle: nil)
+        presenter.configure(with: self)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -37,10 +44,67 @@ class CountryViewController: UIViewController {
     }
 
     func setupUI() {
+        self.view.addSubview(loader)
+        loader.center = view.center
         self.view.backgroundColor = .white
         self.view.addSubview(itemsTableView)
         itemsTableView.edgeAnchors == view.edgeAnchors
+        itemsTableView.dataSource = self
     }
 }
 
 
+extension CountryViewController: CountryView {
+    func showError(error: APIError) {
+        DispatchQueue.main.async {
+            self.showAlert(title: "Error", message: error.localizedDescription)
+        }
+        
+    }
+    
+    func setScreenTitle(title: String) {
+        DispatchQueue.main.async {
+            self.title = title
+        }
+    }
+    
+    func setViewModels(_ viewModels: [ItemModel]) {
+        self.viewModels = viewModels
+        DispatchQueue.main.async { [weak self] in
+            self?.itemsTableView.reloadData()
+        }
+    }
+    
+    func showLoading() {
+        loader.startAnimating()
+    }
+    
+    func hideLoading() {
+        DispatchQueue.main.async { [weak self] in
+            self?.loader.stopAnimating()
+        }
+    }
+    
+    
+}
+
+extension CountryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModels?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ItemTableViewCell", for: indexPath) as? ItemTableViewCell, let item = viewModels?[indexPath.row] else { return UITableViewCell() }
+        cell.configureCell(item: item)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+}
