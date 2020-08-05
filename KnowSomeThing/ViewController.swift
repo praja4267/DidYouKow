d19b6ba85e85119faba1632a372043962dcc55e2
Rajasekhar <praja4267@gmail.com>
2020-08-05 09:01:18 +0530

    Initial commit

---
+++ KnowSomeThing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KnowSomeThing
+//
+//  Created by Rajasekhar on 05/08/20.
+//  Copyright © 2020 Rajasekhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
