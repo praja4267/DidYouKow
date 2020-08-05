c4c7987a95b1200606a93aa0241d26e35ce2f1aa
Rajasekhar <praja4267@gmail.com>
2020-08-05 17:30:47 +0530

    Added utils for Alert, Avatar and Color

---
+++ KnowSomeThing/Utils/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  KnowSomeThing
+//
+//  Created by Rajasekhar on 05/08/20.
+//  Copyright © 2020 Rajasekhar. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let lightYellow = UIColor(red:1, green:0.94, blue:0.23, alpha:1)
+    static let defaultBlue = UIColor(red: 0, green: 0.41, blue: 0.56, alpha: 1.0)
+}
---
+++ KnowSomeThing/Utils/ViewController+extensions.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController+extensions.swift
+//  KnowSomeThing
+//
+//  Created by Rajasekhar on 05/08/20.
+//  Copyright © 2020 Rajasekhar. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    @nonobjc func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+}
